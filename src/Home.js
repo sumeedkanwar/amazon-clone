8495c77df0cb33031acbe395b73b372fcd9997ba
Sumeed Kanwar <sumeedkanwar@gmail.com>
2024-01-29 16:12:23 +0500

    "working on home"

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import './Home.css'
+import home_image from './images/home_image.jpg'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+    <div className='home__container'>
+      <img src={home_image} alt='' className='home__image' />
+    </div>
+
+    <div className='home__row'>
+      {/* Product */}
+      {/* Product */}
+    </div>
+
+    <div className='home__row'>
+      {/* Product */}
+      {/* Product */}
+      {/* Product */}
+    </div>
+
+    <div className='home__row'>
+      {/* Product */}
+    </div>
+
+    </div>
   )
 }
 
---
+++ src/Product.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Product.css'
+
+function Product() {
+  return (
+    <div className='product'>
+        <div className='product__info'>
+            <p>The lean startup</p>
+            <p className='product__price'>
+            <small>$</small>
+            <strong>19.99</strong>
+            </p>
+            <div className='product__rating'>
+            <p>⭐</p>
+            </div>
+        </div>
+        <img src='' alt='' />
+        <button>Add to Basket</button>
+    </div>
+  )
+}
+
+export default Product;
